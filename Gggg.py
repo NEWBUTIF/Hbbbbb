fcc4c37626ce3cea554ec6ab3382fdf0c6409bcc
NEWBUTIF <101060210+NEWBUTIF@users.noreply.github.com>
2024-07-05 22:58:08 +0300

    Update Gggg.py

--- Gggg.py
+++ Gggg.py
@@ -1,6 +1,6 @@
 from hikkatl.types import Message
 from .. import loader, utils
-from telethon import functions, types
+from telethon import functions, types, events
 
 @loader.tds
 class MyModule(loader.Module):
@@ -15,10 +15,12 @@ class MyModule(loader.Module):
         "list_cleared": "Monitored users list cleared",
         "monitoring_users": "Monitored users: {0}",
         "no_users": "No users are being monitored",
-        "group_created": "Group {0} created for logging",
         "group_set": "Group {0} set for logging",
         "current_group": "Current logging group: {0}",
-        "no_group_set": "No logging group is set"
+        "no_group_set": "No logging group is set",
+        "monitoring_enabled": "Monitoring enabled",
+        "monitoring_disabled": "Monitoring disabled",
+        "monitoring_status": "Monitoring is currently {0}"
     }
 
     async def client_ready(self, client, db):
@@ -26,6 +28,10 @@ class MyModule(loader.Module):
         self._db = db
         self.monitored_users = self._db.get("MyModule", "monitored_users", [])
         self.logging_group = self._db.get("MyModule", "logging_group", None)
+        self.monitoring_enabled = self._db.get("MyModule", "monitoring_enabled", True)
+
+        if self.monitoring_enabled:
+            self._client.add_event_handler(self.on_user_update, events.UserUpdate)
 
     @loader.command(
         ru_doc="Устанавливает пользователя для мониторинга по тегу с указанием заметки",
@@ -85,24 +91,6 @@ class MyModule(loader.Module):
         await utils.answer(message, self.strings("monitoring_users").format(users_list))
 
     @loader.command(
-        ru_doc="Создает группу для логирования",
-    )
-    async def cguser(self, message: Message):
-        """Creates a group for logging"""
-        args = utils.get_args_raw(message)
-        if not args:
-            return await utils.answer(message, "Please provide a group name")
-        
-        result = await self._client(functions.messages.CreateChatRequest(
-            users=["me"],
-            title=args
-        ))
-        group_id = result.chats[0].id
-        self._db.set("MyModule", "logging_group", group_id)
-        self.logging_group = group_id
-        await utils.answer(message, self.strings("group_created").format(args))
-
-    @loader.command(
         ru_doc="Устанавливает группу для логирования",
     )
     async def setgroup(self, message: Message):
@@ -128,3 +116,36 @@ class MyModule(loader.Module):
             await utils.answer(message, self.strings("current_group").format(self.logging_group))
         else:
             await utils.answer(message, self.strings("no_group_set"))
+
+    @loader.command(
+        ru_doc="Включает мониторинг",
+    )
+    async def enablemonitoring(self, message: Message):
+        """Enables monitoring"""
+        self.monitoring_enabled = True
+        self._db.set("MyModule", "monitoring_enabled", self.monitoring_enabled)
+        self._client.add_event_handler(self.on_user_update, events.UserUpdate)
+        await utils.answer(message, self.strings("monitoring_enabled"))
+
+    @loader.command(
+        ru_doc="Отключает мониторинг",
+    )
+    async def disablemonitoring(self, message: Message):
+        """Disables monitoring"""
+        self.monitoring_enabled = False
+        self._db.set("MyModule", "monitoring_enabled", self.monitoring_enabled)
+        self._client.remove_event_handler(self.on_user_update, events.UserUpdate)
+        await utils.answer(message, self.strings("monitoring_disabled"))
+
+    @loader.command(
+        ru_doc="Показывает статус мониторинга",
+    )
+    async def monitorstatus(self, message: Message):
+        """Shows monitoring status"""
+        status = "enabled" if self.monitoring_enabled else "disabled"
+        await utils.answer(message, self.strings("monitoring_status").format(status))
+
+    async def on_user_update(self, event: events.UserUpdate.Event):
+        if event.user_id in [user["id"] for user in self.monitored_users]:
+            # Логика для отслеживания статуса пользователя и записи в группу
+            pass
